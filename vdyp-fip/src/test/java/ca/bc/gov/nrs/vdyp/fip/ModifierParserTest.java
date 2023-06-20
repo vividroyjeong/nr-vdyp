ff21e3a1146b0a51ff2fb72b3091facabadc6bc5
Kevin Smith <ksmith@vividsolutions.com>
2023-06-20 11:08:45 -0700

    Handle missing control file

--- vdyp-fip/src/test/java/ca/bc/gov/nrs/vdyp/fip/ModifierParserTest.java
+++ vdyp-fip/src/test/java/ca/bc/gov/nrs/vdyp/fip/ModifierParserTest.java
@@ -14,14 +14,15 @@ import java.util.Optional;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import ca.bc.gov.nrs.vdyp.io.FileResolver;
 
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class ModifierParserTest {
 
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Test
 	public void testNoFilenameForControlFile() throws Exception {
 		var parser = new ModifierParser();
@@ -49,6 +50,36 @@ public class ModifierParserTest {
 		
 		assertThat(controlMap, Matchers.aMapWithSize(1));
 		assertThat(controlMap, (Matcher) hasEntry(is(ModifierParser.CONTROL_KEY), notPresent()));
+		
+	}
+	@Test
+	public void testMissingControlFile() throws Exception {
+		var parser = new ModifierParser();
+
+		Map<String, Object> controlMap = new HashMap<>();
+		controlMap.put(ModifierParser.CONTROL_KEY, Optional.of("testFilename"));
+
+		var fileResolver = new FileResolver() {
+
+			@Override
+			public InputStream resolve(String filename) throws IOException {
+				assertThat(filename, is("testFilename"));
+				
+				throw new IOException();
+			}
+
+			@Override
+			public String toString(String filename) throws IOException {
+				fail("Should not call FileResolver::toString");
+				return filename;
+			}
+			
+		};
+		
+		
+		var ex = Assertions.assertThrows(IOException.class, ()->parser.modify(controlMap, fileResolver));
+		
+		assertThat(ex, Matchers.notNullValue());
 		
 	}
 	
