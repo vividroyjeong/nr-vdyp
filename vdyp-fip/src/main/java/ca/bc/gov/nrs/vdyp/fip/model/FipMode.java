9fa8cf8ea8b1f6f089e35be2ef96247747f0415e
Kevin Smith <ksmith@vividsolutions.com>
2023-06-30 13:08:14 -0700

    Parsing polygon data file

---
+++ vdyp-fip/src/main/java/ca/bc/gov/nrs/vdyp/fip/model/FipMode.java
@@ -0,0 +1,33 @@
+package ca.bc.gov.nrs.vdyp.fip.model;
+
+import java.util.Optional;
+
+public enum FipMode {
+
+	DONT_PROCESS(-1), FIPSTART(1), FIPYOUNG(2);
+
+	private final int code;
+
+	FipMode(int i) {
+		code = i;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public static Optional<FipMode> getByCode(int code) {
+		switch (code) {
+		case -1:
+			return Optional.of(DONT_PROCESS);
+		case 0:
+			return Optional.empty();
+		case 1:
+			return Optional.of(FIPSTART);
+		case 2:
+			return Optional.of(FIPYOUNG);
+		default:
+			return Optional.of(DONT_PROCESS);
+		}
+	}
+}
--- vdyp-fip/src/test/java/ca/bc/gov/nrs/vdyp/fip/FipPolygonParserTest.java
+++ vdyp-fip/src/test/java/ca/bc/gov/nrs/vdyp/fip/FipPolygonParserTest.java
@@ -1,7 +1,11 @@
 package ca.bc.gov.nrs.vdyp.fip;
 
+import static ca.bc.gov.nrs.vdyp.test.VdypMatchers.notPresent;
+import static ca.bc.gov.nrs.vdyp.test.VdypMatchers.present;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -15,6 +19,7 @@ import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.jupiter.api.Test;
 
+import ca.bc.gov.nrs.vdyp.fip.model.FipMode;
 import ca.bc.gov.nrs.vdyp.fip.model.FipPolygon;
 import ca.bc.gov.nrs.vdyp.io.parse.BecDefinitionParserTest;
 import ca.bc.gov.nrs.vdyp.io.parse.ResourceParseException;
@@ -60,10 +65,8 @@ public class FipPolygonParserTest {
 		controlMap.put(FipPolygonParser.CONTROL_KEY, "test.dat");
 		BecDefinitionParserTest.populateControlMapReal(controlMap);
 
-		// Polygon ID FIZ BEC PctF mode ND Yld
-		// AAAAAAAAAAAAAAAAAAAAAAAAA A AAAA FFFF II AAAAAFFFFF
 		var fileResolver = TestUtils
-				.fileResolver("test.dat", TestUtils.makeStream("Test Polygon              1 CWH  90.0  1 BLAH  0.95"));
+				.fileResolver("test.dat", TestUtils.makeStream("Test Polygon              A CWH  90.0  2 BLAH  0.95"));
 
 		parser.modify(controlMap, fileResolver);
 
@@ -77,6 +80,14 @@ public class FipPolygonParserTest {
 		assertThat(stream, instanceOf(StreamingParser.class));
 
 		var poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("Test Polygon")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", present(is(90.0f))));
+		assertThat(poly, hasProperty("modeFip", present(is(FipMode.FIPYOUNG))));
+		assertThat(poly, hasProperty("nonproductiveDescription", present(is("BLAH"))));
+		assertThat(poly, hasProperty("yieldFactor", is(0.95f)));
 
 		assertEmpty(stream);
 	}
@@ -91,10 +102,8 @@ public class FipPolygonParserTest {
 		controlMap.put(FipPolygonParser.CONTROL_KEY, "test.dat");
 		BecDefinitionParserTest.populateControlMapReal(controlMap);
 
-		// Polygon ID FIZ BEC PctF mode ND Yld
-		// AAAAAAAAAAAAAAAAAAAAAAAAA A AAAA FFFF II AAAAAFFFFF
 		var fileResolver = TestUtils
-				.fileResolver("test.dat", TestUtils.makeStream("01002 S000001 00     1970 A CWH                1.00"));
+				.fileResolver("test.dat", TestUtils.makeStream("01002 S000001 00     1970 A CWH                    "));
 
 		parser.modify(controlMap, fileResolver);
 
@@ -108,6 +117,228 @@ public class FipPolygonParserTest {
 		assertThat(stream, instanceOf(StreamingParser.class));
 
 		var poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000001 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		assertEmpty(stream);
+	}
+
+	@Test
+	public void testParseMultiple() throws Exception {
+
+		var parser = new FipPolygonParser();
+
+		Map<String, Object> controlMap = new HashMap<>();
+
+		controlMap.put(FipPolygonParser.CONTROL_KEY, "test.dat");
+		BecDefinitionParserTest.populateControlMapReal(controlMap);
+
+		var fileResolver = TestUtils.fileResolver(
+				"test.dat",
+				TestUtils.makeStream(
+						"01002 S000001 00     1970 A CWH                1.00",
+						"01002 S000002 00     1970 A CWH                1.00",
+						"01002 S000003 00     1970 A CWH                1.00",
+						"01002 S000004 00     1970 A CWH                1.00",
+						"01003AS000001 00     1953 B CWH                1.00",
+						"01003AS000003 00     1953 B CWH                1.00",
+						"01004 S000002 00     1953 B CWH                1.00",
+						"01004 S000036 00     1957 B CWH                1.00",
+						"01004 S000037 00     1957 B CWH                1.00",
+						"01004 S000038 00     1957 B CWH                1.00"
+				)
+		);
+
+		parser.modify(controlMap, fileResolver);
+
+		var parserFactory = controlMap.get(FipPolygonParser.CONTROL_KEY);
+
+		assertThat(parserFactory, instanceOf(StreamingParserFactory.class));
+
+		@SuppressWarnings("unchecked")
+		var stream = ((StreamingParserFactory<FipPolygon>) parserFactory).get();
+
+		assertThat(stream, instanceOf(StreamingParser.class));
+
+		var poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000001 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000002 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000003 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000004 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01003AS000001 00     1953")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("B")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01003AS000003 00     1953")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("B")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01004 S000002 00     1953")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("B")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01004 S000036 00     1957")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("B")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01004 S000037 00     1957")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("B")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01004 S000038 00     1957")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("B")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		assertEmpty(stream);
+	}
+
+	@Test
+	public void testParsePolygonZeroAsDefault() throws Exception {
+
+		var parser = new FipPolygonParser();
+
+		Map<String, Object> controlMap = new HashMap<>();
+
+		controlMap.put(FipPolygonParser.CONTROL_KEY, "test.dat");
+		BecDefinitionParserTest.populateControlMapReal(controlMap);
+
+		var fileResolver = TestUtils
+				.fileResolver("test.dat", TestUtils.makeStream("01002 S000001 00     1970 A CWH   0.0  0       0.00"));
+
+		parser.modify(controlMap, fileResolver);
+
+		var parserFactory = controlMap.get(FipPolygonParser.CONTROL_KEY);
+
+		assertThat(parserFactory, instanceOf(StreamingParserFactory.class));
+
+		@SuppressWarnings("unchecked")
+		var stream = ((StreamingParserFactory<FipPolygon>) parserFactory).get();
+
+		assertThat(stream, instanceOf(StreamingParser.class));
+
+		var poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000001 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
+
+		assertEmpty(stream);
+	}
+
+	@Test
+	public void testParsePolygonNegativeAsDefault() throws Exception {
+
+		var parser = new FipPolygonParser();
+
+		Map<String, Object> controlMap = new HashMap<>();
+
+		controlMap.put(FipPolygonParser.CONTROL_KEY, "test.dat");
+		BecDefinitionParserTest.populateControlMapReal(controlMap);
+
+		var fileResolver = TestUtils
+				.fileResolver("test.dat", TestUtils.makeStream("01002 S000001 00     1970 A CWH  -1.0         -1.00"));
+
+		parser.modify(controlMap, fileResolver);
+
+		var parserFactory = controlMap.get(FipPolygonParser.CONTROL_KEY);
+
+		assertThat(parserFactory, instanceOf(StreamingParserFactory.class));
+
+		@SuppressWarnings("unchecked")
+		var stream = ((StreamingParserFactory<FipPolygon>) parserFactory).get();
+
+		assertThat(stream, instanceOf(StreamingParser.class));
+
+		var poly = assertNext(stream);
+
+		assertThat(poly, hasProperty("polygonIdentifier", is("01002 S000001 00     1970")));
+		assertThat(poly, hasProperty("forestInventoryZone", is("A")));
+		assertThat(poly, hasProperty("biogeoclimaticZone", is("CWH")));
+		assertThat(poly, hasProperty("percentAvailable", notPresent()));
+		assertThat(poly, hasProperty("modeFip", notPresent()));
+		assertThat(poly, hasProperty("nonproductiveDescription", notPresent()));
+		assertThat(poly, hasProperty("yieldFactor", is(1.0f)));
 
 		assertEmpty(stream);
 	}
