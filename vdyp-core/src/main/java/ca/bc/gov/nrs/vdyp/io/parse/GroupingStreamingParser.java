a5ffcd49b3013d9417ea5df737becf5f0595cacf
Kevin Smith <ksmith@vividsolutions.com>
2023-07-20 17:41:19 -0700

    Fix warnings in species parser and grouping parser

--- vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/GroupingStreamingParser.java
+++ vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/GroupingStreamingParser.java
@@ -19,7 +19,7 @@ public abstract class GroupingStreamingParser<T, U> implements StreamingParser<T
 
 	Optional<List<U>> next = Optional.empty();
 
-	public GroupingStreamingParser(StreamingParser<U> delegate) {
+	protected GroupingStreamingParser(StreamingParser<U> delegate) {
 		super();
 		this.delegate = delegate;
 	}
@@ -64,7 +64,7 @@ public abstract class GroupingStreamingParser<T, U> implements StreamingParser<T
 
 			var nextChild = safeNextChild();
 			while (nextChild.map(x -> !stop(x)).orElse(false)) {
-				nextResult.add(nextChild.get());
+				nextChild.ifPresent(nextResult::add); // Should always be present due to loop condition
 				nextChild = safeNextChild();
 			}
 			if (nextChild.isEmpty()) {
