de38531c3a47a82db67d46760941de7dea7974e9
Eric Huber <ehuber@VividSolutions.com>
2023-07-25 13:47:05 -0700

    Fix shouldBranch always been set to False in Age2Age

--- vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/common_calculators/Age2Age.java
+++ vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/common_calculators/Age2Age.java
@@ -91,78 +91,115 @@ public class Age2Age {
 		switch (cu_index) {
 		case SI_ACB_HUANGAC:
 			shouldBranch = true;
+			break;
 		case SI_ACT_THROWERAC:
 			shouldBranch = true;
+			break;
 		case SI_AT_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_BA_KURUCZ82AC:
 			shouldBranch = true;
+			break;
 		case SI_BA_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_BL_CHENAC:
 			shouldBranch = true;
+			break;
 		case SI_BP_CURTISAC:
 			shouldBranch = true;
+			break;
 		case SI_CWC_KURUCZAC:
 			shouldBranch = true;
+			break;
 		case SI_CWI_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_DR_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_EP_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_FDC_BRUCENIGH:
 			shouldBranch = true;
+			break;
 		case SI_FDC_BRUCEAC:
 			shouldBranch = true;
+			break;
 		case SI_FDC_NIGHTA:
 			shouldBranch = true;
+			break;
 		case SI_FDI_THROWERAC:
 			shouldBranch = true;
+			break;
 		case SI_HM_MEANSAC:
 			shouldBranch = true;
+			break;
 		case SI_HWC_WILEYAC:
 			shouldBranch = true;
+			break;
 		case SI_HWI_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_LW_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_PJ_HUANG:
 			shouldBranch = true;
+			break;
 		case SI_PJ_HUANGAC:
 			shouldBranch = true;
+			break;
 		case SI_PLI_NIGHTA2004:
 			shouldBranch = true;
+			break;
 		case SI_PLI_NIGHTA98:
 			shouldBranch = true;
+			break;
 		case SI_PLI_THROWNIGH:
 			shouldBranch = true;
+			break;
 		case SI_PLI_THROWER:
 			shouldBranch = true;
+			break;
 		case SI_PW_CURTISAC:
 			shouldBranch = true;
+			break;
 		case SI_PY_HANNAC:
 			shouldBranch = true;
+			break;
 		case SI_PY_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_SB_NIGH:
 			shouldBranch = true;
+			break;
 		case SI_SE_CHENAC:
 			shouldBranch = true;
+			break;
 		case SI_SE_NIGHTA:
 			shouldBranch = true;
+			break;
 		case SI_SW_GOUDIE_NATAC:
 			shouldBranch = true;
+			break;
 		case SI_SW_GOUDIE_PLAAC:
 			shouldBranch = true;
+			break;
 		case SI_SW_GOUDNIGH:
 			shouldBranch = true;
+			break;
 		case SI_SW_NIGHTA2004:
 			shouldBranch = true;
+			break;
 		case SI_SW_NIGHTA:
 			shouldBranch = true;
+			break;
 		case SI_SS_NIGH:
 			shouldBranch = true;
+			break;
 		default:
 			shouldBranch = false;
 		}
