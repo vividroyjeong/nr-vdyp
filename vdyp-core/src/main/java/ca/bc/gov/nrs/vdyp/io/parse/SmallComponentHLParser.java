ed4199ae83c5ed371c0f9969a31fbed498279d89
Michael Junkin <michael.junkin@gmail.com>
2024-01-10 10:51:55 -0800

    Added file definition comments to "final" parser classes

--- vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/SmallComponentHLParser.java
+++ vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/SmallComponentHLParser.java
@@ -1,5 +1,26 @@
 package ca.bc.gov.nrs.vdyp.io.parse;
 
+import java.util.Map;
+
+/**
+ * Parser for a Small HL Component data file. Each line contains a Species code
+ * and an array of two coefficients (one-based).
+ * <ol>
+ * <li>(cols 0-2) key - a Species code</li>
+ * <li>(col 3-12, 13-22) - float - coefficients</li>
+ * </ol>
+ * Lines with an empty Species code are considered blank and are skipped. All lines in
+ * the file are parsed.
+ * <p>
+ * The result of the parse is a {@link Map} of Species codes to a set of two coefficients.
+ * <p>
+ * FIP Control index: 085
+ * <p>
+ * Example file: coe/REGHL1C.DAT
+ *
+ * @author Kevin Smith, Vivid Solutions
+ * @see SimpleCoefficientParser1
+ */
 public class SmallComponentHLParser extends SimpleCoefficientParser1<String> {
 
 	public static final String CONTROL_KEY = "SMALL_COMP_HL";
--- vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/VeteranLayerVolumeAdjustParser.java
+++ vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/VeteranLayerVolumeAdjustParser.java
@@ -1,5 +1,27 @@
 package ca.bc.gov.nrs.vdyp.io.parse;
 
+import java.util.Map;
+
+/**
+ * Parser for a Veteran Layer Volume Adjust data file. Each line contains a Species code
+ * and an array of four coefficients (one-based).
+ * <ol>
+ * <li>(cols 0-1) key - a species code</li>
+ * <li>(col 2-10, 11-19, 20-28, 29-37) - float - coefficients</li>
+ * </ol>
+ * Exactly 16 data items (one per species) must be present. There can be no blank lines 
+ * in the file, except possibly the last (17th) line provided its empty.
+ * <p>
+ * The result of the parse is a {@link Map} of Species aliases (e.g., "AC") to a set of 
+ * four coefficients.
+ * <p>
+ * FIP Control index: 096
+ * <p>
+ * Example file: coe/VETVOL1.DAT
+ *
+ * @author Kevin Smith, Vivid Solutions
+ * @see SimpleCoefficientParser1
+ */
 public class VeteranLayerVolumeAdjustParser extends SimpleCoefficientParser1<String> {
 
 	public static final String CONTROL_KEY = "VETERAN_LAYER_VOLUME_ADJUST";
