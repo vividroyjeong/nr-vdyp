41cbf658ce7c287dd35c7608f8b793ac809c036e
Michael Junkin <michael.junkin@gmail.com>
2024-01-23 13:53:14 -0800

    BasalAreaGrowthEmpiricalParser

--- vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/BasalAreaGrowthEmpiricalParser.java
+++ vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/io/parse/BasalAreaGrowthEmpiricalParser.java
@@ -9,6 +9,25 @@ import ca.bc.gov.nrs.vdyp.model.Coefficients;
 import ca.bc.gov.nrs.vdyp.model.MatrixMap2;
 import ca.bc.gov.nrs.vdyp.model.MatrixMap2Impl;
 
+/**
+ * Parses a mapping from a BEC Zone Alias and Species to a list of eight coefficients. Each row contains 
+ * <ol>
+ * <li>(cols 0-3) BEC Zone Alias</li>
+ * <li>(col 6) int - index into the zero-based coefficient list (range 0-7)</li>
+ * <li>(cols 7-9) int - indicator. 0 = process only the first coefficient; 1 = process all coefficients</li>
+ * <li>(cols 10-17, 18-25, 26-33 ...) float * 16 - coefficient list, by species ordinal (AC = 0, AT = 1, etc)</li>
+ * </ol>
+ * All lines are parsed. A BEC Zone Alias value, trimmed, of "" results in the line being skipped.
+ * <p>
+ * The result of the parse is a map from BEC Zone Alias and Species to a (zero-based) eight-element coefficient array.
+ * <p>
+ * FIP Control index: 121
+ * <p>
+ * Example file: coe/GROWBA27.COE
+ *
+ * @author Michael Junkin, Vivid Solutions
+ * @see ControlMapSubResourceParser
+ */
 public class BasalAreaGrowthEmpiricalParser implements ControlMapSubResourceParser<MatrixMap2<String, String, Coefficients>> {
 
 	public static final String CONTROL_KEY = "BA_GROWTH_EMPIRICAL";
