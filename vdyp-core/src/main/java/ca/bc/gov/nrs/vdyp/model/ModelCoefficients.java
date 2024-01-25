d440154dfbd652d6258e7ab98ac9de2b9f66fb79
Michael Junkin <michael.junkin@gmail.com>
2024-01-25 09:38:25 -0800

    PrimarySpeciesBasalAreaGrowthParser

---
+++ vdyp-core/src/main/java/ca/bc/gov/nrs/vdyp/model/ModelCoefficients.java
@@ -0,0 +1,23 @@
+package ca.bc.gov.nrs.vdyp.model;
+
+public class ModelCoefficients
+{
+	private final int model;
+	private final Coefficients coefficients;
+	
+	public ModelCoefficients(int model, Coefficients coefficients)
+	{
+		this.model = model;
+		this.coefficients = coefficients;
+	}
+
+	public int getModel()
+	{
+		return model;
+	}
+
+	public Coefficients getCoefficients()
+	{
+		return coefficients;
+	}
+}
