43fc9b65277a7245bf910eeb44a3b64cb13c855f
Kevin Smith <ksmith@vividsolutions.com>
2024-03-06 13:00:56 -0800

    Remove redundant tests.

--- vdyp-vri/src/test/java/ca/bc/gov/nrs/vdyp/vri/VriSiteParserTest.java
+++ vdyp-vri/src/test/java/ca/bc/gov/nrs/vdyp/vri/VriSiteParserTest.java
@@ -105,7 +105,6 @@ public class VriSiteParserTest {
 		assertEmpty(stream);
 	}
 
-	@Disabled("TODO Convert from Species to Site")
 	@Test
 	public void testIgnoreIfNotPrimaryOrSecondary() throws Exception {
 
@@ -119,9 +118,9 @@ public class VriSiteParserTest {
 		var fileResolver = TestUtils.fileResolver(
 				"test.dat",
 				TestUtils.makeInputStream(
-						"01002 S000001 00     1970 X B  100.0B  100.0     0.0     0.0     0.0",
-						"01002 S000001 00     1970 1 B  100.0B  100.0     0.0     0.0     0.0",
-						"01002 S000001 00     1970 Z      0.0     0.0     0.0     0.0     0.0"
+						"082F074/0071         2001 X 100 28.0 14.3        C CW 10.9          189.1 11",
+						"082F074/0071         2001 P 200 28.0 14.3        C CW 10.9          189.1 11",
+						"082F074/0071         2001 Z   0  0.0  0.0"
 				)
 		);
 
@@ -139,109 +138,17 @@ public class VriSiteParserTest {
 		var genera = assertNext(stream);
 
 		assertThat(
-				genera,
-				containsInAnyOrder(
+				genera, containsInAnyOrder(
 						allOf(
-								hasProperty("polygonIdentifier", is("01002 S000001 00     1970")),
-								hasProperty("layer", is(LayerType.PRIMARY)), hasProperty("genus", is("B")),
-								hasProperty("percentGenus", is(100.0f)),
-								hasProperty("speciesPercent", allOf(aMapWithSize(1), hasSpecificEntry("B", is(100.0f))))
-						)
-				)
-		);
+								hasProperty("ageTotal", present(closeTo(200.0f))), //
+								hasProperty("height", present(closeTo(28.0f))), //
+								hasProperty("siteIndex", present(closeTo(14.3f))), //
+								hasProperty("siteGenus", is("C")), //
+								hasProperty("siteSpecies", is("CW")), //
+								hasProperty("yearsToBreastHeight", present(closeTo(10.9f))), //
+								hasProperty("breastHeightAge", present(closeTo(189.1f))), //
+								hasProperty("siteCurveNumber", present(is(11)))
 
-		assertEmpty(stream);
-	}
-
-	@Disabled("TODO Convert from Species to Site")
-	@Test
-	public void testIgnoreIfPercentIsZero() throws Exception {
-
-		var parser = new VriSiteParser();
-
-		Map<String, Object> controlMap = new HashMap<>();
-
-		controlMap.put(ControlKey.VRI_YIELD_SPEC_DIST_INPUT.name(), "test.dat");
-		TestUtils.populateControlMapGenusReal(controlMap);
-
-		var fileResolver = TestUtils.fileResolver(
-				"test.dat",
-				TestUtils.makeInputStream(
-						"01002 S000001 00     1970 1 C    0.0C  100.0     0.0     0.0     0.0",
-						"01002 S000001 00     1970 1 B  100.0B  100.0     0.0     0.0     0.0",
-						"01002 S000001 00     1970 Z      0.0     0.0     0.0     0.0     0.0"
-				)
-		);
-
-		parser.modify(controlMap, fileResolver);
-
-		var parserFactory = controlMap.get(ControlKey.VRI_YIELD_SPEC_DIST_INPUT.name());
-
-		assertThat(parserFactory, instanceOf(StreamingParserFactory.class));
-
-		@SuppressWarnings("unchecked")
-		var stream = ((StreamingParserFactory<Collection<VriSite>>) parserFactory).get();
-
-		assertThat(stream, instanceOf(StreamingParser.class));
-
-		var genera = assertNext(stream);
-
-		assertThat(
-				genera,
-				containsInAnyOrder(
-						allOf(
-								hasProperty("polygonIdentifier", is("01002 S000001 00     1970")),
-								hasProperty("layer", is(LayerType.PRIMARY)), hasProperty("genus", is("B")),
-								hasProperty("percentGenus", is(100.0f)),
-								hasProperty("speciesPercent", allOf(aMapWithSize(1), hasSpecificEntry("B", is(100.0f))))
-						)
-				)
-		);
-
-		assertEmpty(stream);
-	}
-
-	@Disabled("TODO Convert from Species to Site")
-	@Test
-	public void testIgnoreIfPercentIsNegative() throws Exception {
-
-		var parser = new VriSiteParser();
-
-		Map<String, Object> controlMap = new HashMap<>();
-
-		controlMap.put(ControlKey.VRI_YIELD_SPEC_DIST_INPUT.name(), "test.dat");
-		TestUtils.populateControlMapGenusReal(controlMap);
-
-		var fileResolver = TestUtils.fileResolver(
-				"test.dat",
-				TestUtils.makeInputStream(
-						"01002 S000001 00     1970 1 C   -1.0C  100.0     0.0     0.0     0.0",
-						"01002 S000001 00     1970 1 B  100.0B  100.0     0.0     0.0     0.0",
-						"01002 S000001 00     1970 Z      0.0     0.0     0.0     0.0     0.0"
-				)
-		);
-
-		parser.modify(controlMap, fileResolver);
-
-		var parserFactory = controlMap.get(ControlKey.VRI_YIELD_SPEC_DIST_INPUT.name());
-
-		assertThat(parserFactory, instanceOf(StreamingParserFactory.class));
-
-		@SuppressWarnings("unchecked")
-		var stream = ((StreamingParserFactory<Collection<VriSite>>) parserFactory).get();
-
-		assertThat(stream, instanceOf(StreamingParser.class));
-
-		var genera = assertNext(stream);
-
-		assertThat(
-				genera,
-				containsInAnyOrder(
-						allOf(
-								hasProperty("polygonIdentifier", is("01002 S000001 00     1970")),
-								hasProperty("layer", is(LayerType.PRIMARY)), hasProperty("genus", is("B")),
-								hasProperty("percentGenus", is(100.0f)),
-								hasProperty("speciesPercent", allOf(aMapWithSize(1), hasSpecificEntry("B", is(100.0f))))
 						)
 				)
 		);
