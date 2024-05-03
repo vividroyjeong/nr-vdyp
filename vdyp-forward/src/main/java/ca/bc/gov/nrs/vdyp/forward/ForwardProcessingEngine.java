ed456178e02008711ba509a1074d7930e509a0e8
Michael Junkin <michael.junkin@gmail.com>
2024-05-02 17:25:24 -0700

    Implement findPrimarySpecies and all supporting methods

--- vdyp-forward/src/main/java/ca/bc/gov/nrs/vdyp/forward/ForwardProcessingEngine.java
+++ vdyp-forward/src/main/java/ca/bc/gov/nrs/vdyp/forward/ForwardProcessingEngine.java
@@ -1,10 +1,14 @@
 package ca.bc.gov.nrs.vdyp.forward;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +21,7 @@ import ca.bc.gov.nrs.vdyp.forward.model.VdypEntity;
 import ca.bc.gov.nrs.vdyp.forward.model.VdypPolygon;
 import ca.bc.gov.nrs.vdyp.model.BecDefinition;
 import ca.bc.gov.nrs.vdyp.model.BecLookup;
+import ca.bc.gov.nrs.vdyp.model.CommonData;
 import ca.bc.gov.nrs.vdyp.model.GenusDefinition;
 import ca.bc.gov.nrs.vdyp.model.GenusDistribution;
 import ca.bc.gov.nrs.vdyp.model.LayerType;
@@ -68,55 +73,66 @@ public class ForwardProcessingEngine {
 		validatePolygon(polygon);
 
 		PolygonProcessingState workingBank = fps.getBank(LayerType.PRIMARY, 0).copy();
-		
+
 		// SCINXSET
 		completeSiteCurveMap(polygon.getBiogeoclimaticZone(), workingBank);
 
 		// VPRIME, method == 1
 		setSpecies(workingBank);
-		
+
 		fps.setActive(LayerType.PRIMARY, 0);
 	}
-	
-	private void setSpecies(PolygonProcessingState bank) {
-		
-		logger.atDebug().addArgument(bank.getNSpecies()).addArgument(bank.basalArea[0][0])
-			.log("Calculating Pcts as a ratio of Species BA over Total BA. # species: {}; Layer total 7.5cm+ basal area: {}");
-		
-		for (int i = 0; i < bank.getNSpecies(); i++) {
-			bank.percentForestedLand[i] = bank.basalArea[i][0] / bank.basalArea[0][0] * 100.0f;
 
-			logger.atDebug().addArgument(i).addArgument(bank.speciesIndex[i]).addArgument(bank.speciesName[i])
-				.addArgument(bank.basalArea[i][0]).addArgument(bank.percentForestedLand[i])
-				.log("Species {}: SP0 {}, Name {}, Species 7.5cm+ BA {}, Calculated Percent {}");
+	private static void setSpecies(PolygonProcessingState bank) {
+
+		setPercentages(bank);
+	}
+
+	public static void setPercentages(PolygonProcessingState bank) {
+		logger.atDebug().addArgument(bank.getNSpecies()).addArgument(bank.basalAreas[0][0])
+				.log(
+						"Calculating percents as a ratio of Species BA over Total BA. # species: {}; Layer total 7.5cm+ basal area: {}"
+				);
+
+		int ucIndex = UtilizationClass.ALL.ordinal();
+		for (int i = 1; i <= bank.getNSpecies(); i++) {
+			bank.percentagesOfForestedLand[i] = bank.basalAreas[i][ucIndex] / bank.basalAreas[0][ucIndex] * 100.0f;
+
+			logger.atDebug().addArgument(i).addArgument(bank.speciesIndices[i]).addArgument(bank.speciesNames[i])
+					.addArgument(bank.basalAreas[i][0]).addArgument(bank.percentagesOfForestedLand[i])
+					.log("Species {}: SP0 {}, Name {}, Species 7.5cm+ BA {}, Calculated Percent {}");
 		}
 	}
 
-	private void completeSiteCurveMap(BecDefinition becZone, PolygonProcessingState workingBank) {
+	private void completeSiteCurveMap(BecDefinition becZone, PolygonProcessingState bank) {
 
-		for (int i = 0; i < workingBank.getNSpecies(); i++) {
+		for (int i = 0; i < bank.getNSpecies(); i++) {
 
-			if (workingBank.siteCurveNumber[i] == VdypEntity.MISSING_INTEGER_VALUE) {
+			if (bank.siteCurveNumbers[i] == VdypEntity.MISSING_INTEGER_VALUE) {
 
 				Optional<SiteIndexEquation> scIndex = Optional.empty();
-				
-				Optional<GenusDistribution> sp0Dist = workingBank.sp64Distribution[i].getSpeciesDistribution(0);
+
+				Optional<GenusDistribution> sp0Dist = bank.sp64Distributions[i].getSpeciesDistribution(0);
 
 				if (sp0Dist.isPresent()) {
 					if (siteCurveMap.size() > 0) {
 						SiteCurve sc = siteCurveMap.get(sp0Dist.get().getGenus().getAlias());
 						scIndex = Optional.of(sc.getValue(becZone.getRegion()));
 					} else {
-						scIndex = Optional.of(SiteTool.getSICurve(workingBank.speciesName[i], becZone.getRegion().equals(Region.COASTAL)));
+						scIndex = Optional.of(
+								SiteTool.getSICurve(bank.speciesNames[i], becZone.getRegion().equals(Region.COASTAL))
+						);
 					}
 				}
-				
+
 				if (scIndex.isEmpty()) {
 					if (siteCurveMap.size() > 0) {
-						SiteCurve sc = siteCurveMap.get(workingBank.speciesName[i]);
+						SiteCurve sc = siteCurveMap.get(bank.speciesNames[i]);
 						scIndex = Optional.of(sc.getValue(becZone.getRegion()));
 					} else {
-						scIndex = Optional.of(SiteTool.getSICurve(workingBank.speciesName[i], becZone.getRegion().equals(Region.COASTAL)));
+						scIndex = Optional.of(
+								SiteTool.getSICurve(bank.speciesNames[i], becZone.getRegion().equals(Region.COASTAL))
+						);
 					}
 				}
 			}
@@ -129,8 +145,8 @@ public class ForwardProcessingEngine {
 
 			throw new ProcessingException(
 					MessageFormat.format(
-							"Polygon {}'s year value {} is < 1900", 101, polygon.getDescription().getName(),
-							polygon.getDescription().getYear()
+							"Polygon {}'s year value {} is < 1900", 101, polygon.getDescription().getName(), polygon
+									.getDescription().getYear()
 					)
 			);
 		}
@@ -142,15 +158,266 @@ public class ForwardProcessingEngine {
 
 		PolygonProcessingState pps = fps.getBank(LayerType.PRIMARY, 0);
 
-		pps.removeSpecies(i -> pps.basalArea[i][UtilizationClass.ALL.ordinal()] < MIN_BASAL_AREA);
-		
+		pps.removeSpecies(i -> pps.basalAreas[i][UtilizationClass.ALL.ordinal()] < MIN_BASAL_AREA);
+
 		if (pps.getNSpecies() == 0) {
-				throw new ProcessingException(
-						MessageFormat.format(
-								"Polygon {0} layer 0 has no species with basal area above {1}",
-								polygon.getDescription().getName(), MIN_BASAL_AREA
-						)
-				);
+			throw new ProcessingException(
+					MessageFormat.format(
+							"Polygon {0} layer 0 has no species with basal area above {1}", polygon.getDescription()
+									.getName(), MIN_BASAL_AREA
+					)
+			);
+		}
+	}
+
+	public static record SpeciesRankingDetails(
+			int primarySpeciesIndex, Optional<Integer> secondarySpeciesIndex, int inventoryTypeGroup
+	) {
+	}
+
+	// PRIMFIND
+	/**
+	 * Returns a {@code SpeciesRankingDetails} instance giving:
+	 * <ul>
+	 * <li>the index in {@code bank} of the primary species
+	 * <li>the index in {@code bank} of the secondary species, or Optional.empty() if none, and
+	 * <li>the percentage of forested land occupied by the primary species
+	 * </ul>
+	 * @param bank the bank on which to operate
+	 * @return as described
+	 */
+	static SpeciesRankingDetails findPrimarySpecies(PolygonProcessingState bank, Collection<List<String>> speciesToCombine) {
+
+		if (bank.getNSpecies() == 0) {
+			throw new IllegalArgumentException("Can not find primary species as there are no species");
+		}
+
+		float percentages[] = Arrays.copyOf(bank.percentagesOfForestedLand, bank.percentagesOfForestedLand.length);
+
+		for (var speciesPair : speciesToCombine) {
+			combinePercentages(bank.speciesNames, speciesPair, percentages);
+		}
+
+		float highestPercentage = 0.0f;
+		int highestPercentageIndex = -1;
+		float secondHighestPercentage = 0.0f;
+		int secondHighestPercentageIndex = -1;
+		for (int i = 0; i < percentages.length; i++) {
+
+			if (percentages[i] > highestPercentage) {
+
+				secondHighestPercentageIndex = highestPercentageIndex;
+				secondHighestPercentage = highestPercentage;
+				highestPercentageIndex = i;
+				highestPercentage = percentages[i];
+				
+			} else if (percentages[i] > secondHighestPercentage) {
+
+				secondHighestPercentageIndex = i;
+				secondHighestPercentage = percentages[i];
+			}
+
+			// TODO: implement NDEBUG22 = 1 logic
+		}
+
+		if (highestPercentageIndex == -1) {
+			throw new IllegalStateException("There are no species with covering percentage > 0");
+		}
+
+		String primaryGenusName = bank.speciesNames[highestPercentageIndex];
+		Optional<String> secondaryGenusName = secondHighestPercentageIndex != -1
+				? Optional.of(bank.speciesNames[secondHighestPercentageIndex])
+				: Optional.empty();
+
+		try {
+			int inventoryTypeGroup = findInventoryTypeGroup(primaryGenusName, secondaryGenusName, highestPercentage);
+
+			return new SpeciesRankingDetails(
+					highestPercentageIndex, secondHighestPercentageIndex != -1
+							? Optional.of(secondHighestPercentageIndex)
+							: Optional.empty(),
+					inventoryTypeGroup
+			);
+		} catch (ProcessingException e) {
+			// This should never fail because the bank has already been validated and hence the genera
+			// are known to be valid.
+
+			throw new IllegalStateException(e);
+		}
+	}
+
+	static void combinePercentages(String[] speciesNames, List<String> combinationGroup, float[] percentages) {
+
+		if (combinationGroup.size() != 2) {
+			throw new IllegalArgumentException(
+					MessageFormat.format("combinationGroup must have size 2; it has size", combinationGroup.size())
+			);
+		}
+
+		if (speciesNames.length != percentages.length) {
+			throw new IllegalArgumentException(
+					MessageFormat.format(
+							"the length of speciesNames ({}) must match that of percentages ({}) but it doesn't", speciesNames.length, percentages.length
+					)
+			);
+		}
+
+		Set<Integer> groupIndices = new HashSet<>();
+		for (int i = 0; i < speciesNames.length; i++) {
+			if (combinationGroup.contains(speciesNames[i]))
+				groupIndices.add(i);
+		}
+
+		if (groupIndices.size() == 2) {
+			Integer[] groupIndicesArray = new Integer[2];
+			groupIndices.toArray(groupIndicesArray);
+
+			int higherPercentageIndex;
+			int lowerPercentageIndex;
+			if (percentages[groupIndicesArray[0]] > percentages[groupIndicesArray[1]]) {
+				higherPercentageIndex = groupIndicesArray[0];
+				lowerPercentageIndex = groupIndicesArray[1];
+			} else {
+				higherPercentageIndex = groupIndicesArray[1];
+				lowerPercentageIndex = groupIndicesArray[0];
+			}
+			percentages[higherPercentageIndex] = percentages[higherPercentageIndex] + percentages[lowerPercentageIndex];
+			percentages[lowerPercentageIndex] = 0.0f;
+		}
+	}
+
+	// ITGFIND
+	/**
+	 * Find Inventory type group (ITG) for the given primary and secondary (if given) genera.
+	 * 
+	 * @param primaryGenus the genus of the primary species
+	 * @param optionalSecondaryGenus the genus of the primary species, which may be empty
+	 * @param primaryPercentage the percentage covered by the primary species
+	 * @return as described
+	 * @throws ProcessingException if primaryGenus is not a known genus
+	 */
+	static int findInventoryTypeGroup(
+			String primaryGenus, Optional<String> optionalSecondaryGenus, float primaryPercentage
+	)
+			throws ProcessingException {
+
+		if (primaryPercentage > 79.999) { // Copied from VDYP7
+
+			Integer recordedInventoryTypeGroup = CommonData.ITG_PURE.get(primaryGenus);
+			if (recordedInventoryTypeGroup == null) {
+				throw new ProcessingException("Unrecognized primary species: " + primaryGenus);
+			}
+
+			return recordedInventoryTypeGroup;
+		}
+
+		String secondaryGenus = optionalSecondaryGenus.isPresent() ? optionalSecondaryGenus.get() : "";
+
+		if (primaryGenus.equals(secondaryGenus)) {
+			throw new IllegalArgumentException("The primary and secondary genera are the same");
+		}
+
+		switch (primaryGenus) {
+		case "F":
+			switch (secondaryGenus) {
+			case "C", "Y":
+				return 2;
+			case "B", "H":
+				return 3;
+			case "S":
+				return 4;
+			case "PL", "PA":
+				return 5;
+			case "PY":
+				return 6;
+			case "L", "PW":
+				return 7;
+			default:
+				return 8;
+			}
+		case "C", "Y":
+			switch (secondaryGenus) {
+			case "H", "B", "S":
+				return 11;
+			default:
+				return 10;
+			}
+		case "H":
+			switch (secondaryGenus) {
+			case "C", "Y":
+				return 14;
+			case "B":
+				return 15;
+			case "S":
+				return 16;
+			default:
+				return 13;
+			}
+		case "B":
+			switch (secondaryGenus) {
+			case "C", "Y", "H":
+				return 19;
+			default:
+				return 20;
+			}
+		case "S":
+			switch (secondaryGenus) {
+			case "C", "Y", "H":
+				return 23;
+			case "B":
+				return 24;
+			case "PL":
+				return 25;
+			default:
+				if (CommonData.HARDWOODS.contains(secondaryGenus)) {
+					return 26;
+				}
+				return 22;
+			}
+		case "PW":
+			return 27;
+		case "PL", "PA":
+			switch (secondaryGenus) {
+			case "PL", "PA":
+				return 28;
+			case "F", "PW", "L", "PY":
+				return 29;
+			default:
+				if (CommonData.HARDWOODS.contains(secondaryGenus)) {
+					return 31;
+				}
+				return 30;
+			}
+		case "PY":
+			return 32;
+		case "L":
+			switch (secondaryGenus) {
+			case "F":
+				return 33;
+			default:
+				return 34;
+			}
+		case "AC":
+			if (CommonData.HARDWOODS.contains(secondaryGenus)) {
+				return 36;
+			}
+			return 35;
+		case "D":
+			if (CommonData.HARDWOODS.contains(secondaryGenus)) {
+				return 38;
+			}
+			return 37;
+		case "MB":
+			return 39;
+		case "E":
+			return 40;
+		case "AT":
+			if (CommonData.HARDWOODS.contains(secondaryGenus)) {
+				return 42;
+			}
+			return 41;
+		default:
+			throw new ProcessingException("Unrecognized primary species: " + primaryGenus);
 		}
 	}
 }
--- vdyp-forward/src/test/java/ca/bc/gov/nrs/vdyp/forward/ForwardProcessingEngineTest.java
+++ vdyp-forward/src/test/java/ca/bc/gov/nrs/vdyp/forward/ForwardProcessingEngineTest.java
@@ -1,43 +1,71 @@
 package ca.bc.gov.nrs.vdyp.forward;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ca.bc.gov.nrs.vdyp.application.ProcessingException;
 import ca.bc.gov.nrs.vdyp.common.ControlKey;
+import ca.bc.gov.nrs.vdyp.common.GenusDefinitionMap;
 import ca.bc.gov.nrs.vdyp.forward.model.VdypPolygonDescription;
 import ca.bc.gov.nrs.vdyp.forward.test.VdypForwardTestUtils;
 import ca.bc.gov.nrs.vdyp.io.parse.common.ResourceParseException;
+import ca.bc.gov.nrs.vdyp.io.parse.streaming.StreamingParser;
 import ca.bc.gov.nrs.vdyp.io.parse.streaming.StreamingParserFactory;
+import ca.bc.gov.nrs.vdyp.model.CommonData;
+import ca.bc.gov.nrs.vdyp.model.GenusDefinition;
+import ca.bc.gov.nrs.vdyp.model.LayerType;
 
 class ForwardProcessingEngineTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(ForwardProcessingEngineTest.class);
 
+	private static ForwardControlParser parser;
+	private static Map<String, Object> controlMap;
+
+	private static StreamingParserFactory<VdypPolygonDescription> polygonDescriptionStreamFactory;
+	private static StreamingParser<VdypPolygonDescription> polygonDescriptionStream;
+
+	private static ForwardDataStreamReader forwardDataStreamReader;
+	private static GenusDefinitionMap genusDefinitionMap;
+
+	@SuppressWarnings("unchecked")
+	@BeforeAll
+	static void beforeTest() throws IOException, ResourceParseException {
+		parser = new ForwardControlParser();
+		controlMap = VdypForwardTestUtils.parse(parser, "VDYP.CTR");
+
+		polygonDescriptionStreamFactory = (StreamingParserFactory<VdypPolygonDescription>) controlMap
+				.get(ControlKey.FORWARD_INPUT_GROWTO.name());
+		polygonDescriptionStream = polygonDescriptionStreamFactory.get();
+
+		forwardDataStreamReader = new ForwardDataStreamReader(controlMap);
+		
+		genusDefinitionMap = new GenusDefinitionMap((List<GenusDefinition>) controlMap.get(ControlKey.SP0_DEF.name()));
+	}
+
 	@Test
 	void test() throws IOException, ResourceParseException, ProcessingException {
-
-		var parser = new ForwardControlParser();
-		var controlMap = VdypForwardTestUtils.parse(parser, "VDYP.CTR");
 
 		ForwardProcessingEngine fpe = new ForwardProcessingEngine(controlMap);
 
 		assertThat(fpe.getBecLookup(), notNullValue());
 		assertThat(fpe.getGenusDefinitionMap(), notNullValue());
 		assertThat(fpe.getSiteCurveMap(), notNullValue());
-
-		@SuppressWarnings("unchecked")
-		var polygonDescriptionStreamFactory = (StreamingParserFactory<VdypPolygonDescription>) controlMap
-				.get(ControlKey.FORWARD_INPUT_GROWTO.name());
-		var polygonDescriptionStream = polygonDescriptionStreamFactory.get();
-
-		var forwardDataStreamReader = new ForwardDataStreamReader(controlMap);
 
 		// Fetch the next polygon to process.
 		int nPolygonsProcessed = 0;
@@ -53,5 +81,171 @@ class ForwardProcessingEngineTest {
 		}
 
 		logger.info("{} polygons processed", nPolygonsProcessed);
+	}
+
+	@Test 
+	void testFindPrimarySpecies() throws IOException, ResourceParseException, ProcessingException {
+		
+		ForwardProcessingState fps = new ForwardProcessingState(genusDefinitionMap);
+
+		var polygonDescription = polygonDescriptionStream.next();
+		var polygon = forwardDataStreamReader.readNextPolygon(polygonDescription);
+		
+		fps.setStartingState(polygon);
+		
+		PolygonProcessingState bank = fps.getBank(LayerType.PRIMARY, 0).copy();
+		
+		ForwardProcessingEngine.setPercentages(bank);
+
+		ForwardProcessingEngine.SpeciesRankingDetails rankingDetails1 = ForwardProcessingEngine.findPrimarySpecies(bank, CommonData.PRIMARY_SPECIES_TO_COMBINE);
+		
+		assertThat(rankingDetails1.primarySpeciesIndex(), is(3));
+		assertThat(rankingDetails1.secondarySpeciesIndex(), is(Optional.of(4)));
+		assertThat(rankingDetails1.inventoryTypeGroup(), is(37));
+
+		var speciesToCombine = Arrays.asList(Arrays.asList(bank.speciesNames[3], bank.speciesNames[4]));
+
+		ForwardProcessingEngine.SpeciesRankingDetails rankingDetails2 = ForwardProcessingEngine.findPrimarySpecies(bank, speciesToCombine);
+		
+		// The test-specific speciesToCombine will combine 3 & 4 into 3 (leaving 4 at 0.0), promoting 2 to secondary.
+		assertThat(rankingDetails2.primarySpeciesIndex(), is(3));
+		assertThat(rankingDetails2.secondarySpeciesIndex(), is(Optional.of(2)));
+		assertThat(rankingDetails2.inventoryTypeGroup(), is(37));
+	}
+	
+	@Test 
+	void testCombinePercentages() {
+		
+		String[] speciesNames = new String[] { "AC",  "B",  "C",  "D",  "E",  "F", "PW",  "H", "PY",  "L", "PA", "AT",  "S", "MB",  "Y", "PL" };
+		float[] percentages = new float[]    { 1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f, 9.3f, 8.4f, 7.1f, 6.3f, 8.4f, 9.4f, 1.6f };
+
+		List<String> combineGroup;
+		float[] testPercentages;
+		
+		combineGroup = List.of("C", "Y");
+		testPercentages = Arrays.copyOf(percentages, percentages.length);
+
+		ForwardProcessingEngine.combinePercentages(speciesNames, combineGroup, testPercentages);
+		
+		assertThat(testPercentages[2], is(0f));
+		assertThat(testPercentages[14], is(12.7f));
+
+		combineGroup = List.of("D", "PL");
+		testPercentages = Arrays.copyOf(percentages, percentages.length);
+
+		ForwardProcessingEngine.combinePercentages(speciesNames, combineGroup, testPercentages);
+		
+		assertThat(testPercentages[3], is(6.0f));
+		assertThat(testPercentages[15], is(0.0f));
+	}
+
+	@Test 
+	void testCombinePercentagesOneGenusNotInCombinationList() {
+		
+		String[] speciesNames = new String[] { "AC", "C",  "D",  "E",  "F", "PW",  "H", "PY",  "L", "PA", "AT",  "S", "MB",  "Y", "PL" };
+		float[] percentages = new float[]    { 1.1f, 3.3f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f, 9.3f, 8.4f, 7.1f, 6.3f, 8.4f, 9.4f, 1.6f };
+
+		List<String> combineGroup = List.of("B", "Y");
+		float[] testPercentages = Arrays.copyOf(percentages, percentages.length);
+
+		ForwardProcessingEngine.combinePercentages(speciesNames, combineGroup, testPercentages);
+		
+		assertThat(testPercentages[13], is(9.4f));
+	}
+
+	@Test 
+	void testCombinePercentagesBothGeneraNotInCombinationList() {
+		
+		String[] speciesNames = new String[] { "AC", "D",  "E",  "F", "PW",  "H", "PY",  "L", "PA", "AT",  "S", "MB",  "Y", "PL" };
+		float[] percentages = new float[]    { 1.1f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f, 9.3f, 8.4f, 7.1f, 6.3f, 8.4f, 9.4f, 1.6f };
+
+		List<String> combineGroup = List.of("B", "C");
+		float[] testPercentages = Arrays.copyOf(percentages, percentages.length);
+
+		ForwardProcessingEngine.combinePercentages(speciesNames, combineGroup, testPercentages);
+		
+		assertThat(testPercentages, is(percentages));
+	}
+
+	@Test 
+	void testCombinePercentagesBadCombinationList() {
+		
+		String[] speciesNames = new String[] { "AC", "D",  "E",  "F", "PW",  "H", "PY",  "L", "PA", "AT",  "S", "MB",  "Y", "PL" };
+		float[] percentages = new float[]    { 1.1f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f, 9.3f, 8.4f, 7.1f, 6.3f, 8.4f, 9.4f, 1.6f };
+
+		List<String> combineGroup = List.of("B", "C", "D");
+
+		try {
+			ForwardProcessingEngine.combinePercentages(speciesNames, combineGroup, percentages);
+			fail();
+		} catch (IllegalArgumentException e) {
+			// expected
+		}
+	}
+
+	@Test 
+	void testCombinePercentagesBadArrays() {
+		
+		String[] speciesNames = new String[] { "D",  "E",  "F", "PW",  "H", "PY",  "L", "PA", "AT",  "S", "MB",  "Y", "PL" };
+		float[] percentages = new float[] { 1.1f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f, 9.3f, 8.4f, 7.1f, 6.3f, 8.4f, 9.4f, 1.6f };
+
+		List<String> combineGroup = List.of("B", "C");
+
+		try {
+			ForwardProcessingEngine.combinePercentages(speciesNames, combineGroup, percentages);
+			fail();
+		} catch (IllegalArgumentException e) {
+			// expected
+		}
+	}
+
+	@Test
+	void testFindInventoryTypeGroup() throws ProcessingException {
+		try {
+			ForwardProcessingEngine.findInventoryTypeGroup("A", Optional.empty(), 80.0f);
+			fail();
+		} catch (ProcessingException e) {
+			// expected
+		}
+
+		assertThrows(
+				IllegalArgumentException.class, () -> ForwardProcessingEngine
+						.findInventoryTypeGroup("AC", Optional.of("AC"), 0.0f)
+		);
+
+		assertThat(ForwardProcessingEngine.findInventoryTypeGroup("AC", Optional.empty(), 80.0f), is(36));
+
+		int[] expectedResults = new int[] { 
+					  /*                  Secondary                         */
+				      /* AC  B  C  D  E  F PW  H PY  L PA AT  S MB  Y PL -- */  
+				/* AC */    35,35,36,36,35,35,35,35,35,35,36,35,36,35,35,35,
+				/*  B */ 20,   19,20,20,20,20,19,20,20,20,20,20,20,19,20,20,
+				/*  C */ 10,11,   10,10,10,10,11,10,10,10,10,11,10,10,10,10,
+				/*  D */ 38,37,37,   38,37,37,37,37,37,37,38,37,38,37,37,37,
+				/*  E */ 40,40,40,40,   40,40,40,40,40,40,40,40,40,40,40,40,
+				/*  F */  8, 3, 2, 8, 8,    7, 3, 6, 7, 5, 8, 4, 8, 2, 5, 8,
+				/* PW */ 27,27,27,27,27,27,   27,27,27,27,27,27,27,27,27,27,
+				/*  H */ 13,15,14,13,13,13,13,   13,13,13,13,16,13,14,13,13,
+				/* PY */ 32,32,32,32,32,32,32,32,   32,32,32,32,32,32,32,32,
+				/*  L */ 34,34,34,34,34,33,34,34,34,   34,34,34,34,34,34,34,
+				/* PA */ 31,30,30,31,31,29,29,30,29,29,   31,30,31,30,28,30,
+				/* AT */ 42,41,41,42,42,41,41,41,41,41,41,   41,42,41,41,41,
+				/*  S */ 26,24,23,26,26,22,22,23,22,22,22,26,   26,23,25,22,
+				/* MB */ 39,39,39,39,39,39,39,39,39,39,39,39,39,   39,39,39,
+				/*  Y */ 10,11,10,10,10,10,10,11,10,10,10,10,11,10,   10,10,
+				/* PL */ 31,30,30,31,31,29,29,30,29,29,28,31,30,31,30,   30 };
+
+		int currentAnswerIndex = 0;
+
+		for (String primaryGenus : CommonData.ITG_PURE.keySet()) {
+			for (Optional<String> secondaryGenus : CommonData.ITG_PURE.keySet().stream()
+					.filter(k -> !k.equals(primaryGenus))
+					.map(k -> Optional.of(k)).collect(Collectors.toList())) {
+				int itg = ForwardProcessingEngine.findInventoryTypeGroup(primaryGenus, secondaryGenus, 50.0f);
+				assertThat(itg, is(expectedResults[currentAnswerIndex++]));
+			}
+			int itg = ForwardProcessingEngine.findInventoryTypeGroup(primaryGenus, Optional.empty(), 50.0f);
+			assertThat(itg, is(expectedResults[currentAnswerIndex++]));
+		}
 	}
 }
