f0b67a29e33010aaf58aa767b43f920cb0f0811f
Roy Jeong <roy.jeong@gov.bc.ca>
2024-09-11 10:05:50 -0700

    Change keycloak init option and remove CSP directive
    remove https://login.microsoftonline.com from frame-src directive

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const KEYCLOAK = Object.freeze({
   PKCE_METHOD: 'S256',
   ONLOAD: 'check-sso',
-  CHECK_LOGIN_IFRAME: false,
+  CHECK_LOGIN_IFRAME: true,
   SILENT_CHECK_SSO_REDIRECT_PAGE: '/silent-check-sso.html',
   IDP_AZUR_IDIR: 'azureidir', // Identity Provider: IDIR with MFA
   MAX_SESSION_DURATION: 8 * 60 * 60 * 1000, // 8 hours (in milliseconds)
