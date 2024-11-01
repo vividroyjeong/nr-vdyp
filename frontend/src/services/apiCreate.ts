c0314c8516b30212aa58e09240159dfaeaab4412
Roy Jeong <roy.jeong@gov.bc.ca>
2024-11-01 14:58:32 -0700

    Remove redundant await

--- frontend/src/services/apiCreate.ts
+++ frontend/src/services/apiCreate.ts
@@ -2,5 +2,20 @@ import { post } from '@/services/apiService'
 import Code from '@/models/code'
 
 export const code = async (code: Code): Promise<any> => {
-  return await post(`/codeTables/${code.codeTableName}`, code)
+  return post(`/codeTables/${code.codeTableName}`, code)
+}
+
+export const uploadFilesWithData = (
+  layerFile: File,
+  polygonFile: File,
+  jsonData: Record<string, any>,
+) => {
+  const formData = new FormData()
+  formData.append('layerFile', layerFile)
+  formData.append('polygonFile', polygonFile)
+  formData.append('jsonData', JSON.stringify(jsonData))
+
+  return post('/projection/dcsv', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  })
 }
