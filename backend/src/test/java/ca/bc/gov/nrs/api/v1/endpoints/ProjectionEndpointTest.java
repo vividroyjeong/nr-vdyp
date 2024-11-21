8e9b4ebd7a301b50e0c35a85cd69bf914b5e6023
Michael Junkin <michael.junkin@gmail.com>
2024-11-21 10:24:10 -0800

    Correct unit test to handle new way of reporting unsupported.

--- backend/src/test/java/ca/bc/gov/nrs/api/v1/endpoints/ProjectionEndpointTest.java
+++ backend/src/test/java/ca/bc/gov/nrs/api/v1/endpoints/ProjectionEndpointTest.java
@@ -25,7 +25,9 @@ import net.datafaker.Faker;
 @QuarkusTest
 class ProjectionEndpointTest {
 
+	@SuppressWarnings("unused")
 	private final TestHelper testHelper;
+	@SuppressWarnings("unused")
 	private final Faker faker = new Faker();
 
 	@Inject
@@ -66,7 +68,7 @@ class ProjectionEndpointTest {
 		request.setVriAdjustInputData(buildTestFile());
 
 		given().basePath("/v8").when().body(request).contentType("application/json").post("/projection/scsv").then()
-				.statusCode(500).body(Matchers.containsString("Not supported"));
+				.statusCode(501);
 	}
 
 	@Test
@@ -77,7 +79,7 @@ class ProjectionEndpointTest {
 		request.setInputData(buildTestFile());
 
 		given().basePath("/v8").when().body(request).contentType("application/json").post("/projection/dcsv").then()
-				.statusCode(500).body(Matchers.containsString("Not supported"));
+				.statusCode(501);
 	}
 
 	private File buildTestFile() throws IOException {
@@ -88,158 +90,4 @@ class ProjectionEndpointTest {
 
 		return tmpFile.toFile();
 	}
-
-//   @Test
-//   void testGetUserById_givenValidID_shouldReturnTheUserAndStatusOK() {
-//     given()r
-//       .basePath("/api/v8")
-//       .pathParam("id", userEntity.getId())
-//       .when().get("/users/{id}")
-//       .then()
-//       .statusCode(200)
-//       .body("name", equalTo(userEntity.getName()))
-//       .body("email", equalTo(userEntity.getEmail()));
-//   }
-
-//   @Test
-//   void testGetUserById_givenRandomID_shouldReturnTheUserAndStatusOK() {
-//     given()
-//       .basePath("/api/v8")
-//       .pathParam("id", 20000)
-//       .when().get("/users/{id}")
-//       .then()
-//       .statusCode(404);
-//   }
-
-//   @Test
-//   void testCreateUser_givenValidPayload_shouldReturnStatusCreated() {
-//     var name = faker.name().fullName();
-//     var email = faker.internet().emailAddress();
-//     User user = new User(null, name, email);
-//     given()
-//       .basePath("/api/v8")
-//       .contentType(ContentType.JSON)
-//       .body(user)
-//       .when().post("/users")
-//       .then()
-//       .statusCode(201)
-//       .body("name", equalTo(name))
-//       .body("email", equalTo(email));
-//   }
-
-//   @Test
-//   void testCreateUser_givenInValidPayload_shouldReturnStatusBadRequest() {
-//     var name = faker.name().fullName();
-//     var email = faker.internet().domainName();
-//     User user = new User(null, name, email);
-//     given()
-//       .basePath("/api/v8")
-//       .contentType(ContentType.JSON)
-//       .body(user)
-//       .when().post("/users")
-//       .then()
-//       .statusCode(400);
-//   }
-
-//   @Test
-//   void testUpdateUser_givenValidPayload_shouldReturnStatusOK() {
-//     var name = faker.name().fullName();
-//     var email = faker.internet().emailAddress();
-//     User user = new User(userEntity.getId(), name, email);
-//     given()
-//       .basePath("/api/v8")
-//       .contentType(ContentType.JSON)
-//       .pathParam("id", userEntity.getId())
-//       .body(user)
-//       .when().put("/users/{id}")
-//       .then()
-//       .statusCode(200)
-//       .body("name", equalTo(user.name()))
-//       .body("email", equalTo(user.email()));
-//   }
-
-//   @Test
-//   void testDeleteUser_givenValidID_shouldReturnStatusNoContent() {
-//     given()
-//       .basePath("/api/v8")
-//       .pathParam("id", userEntity.getId())
-//       .when().delete("/users/{id}")
-//       .then()
-//       .statusCode(204);
-//   }
-
-//   @Test
-//   void testDeleteUser_givenInvalidID_shouldReturnStatusNotFound() {
-//     given()
-//       .basePath("/api/v8")
-//       .pathParam("id", 100003330)
-//       .when().delete("/users/{id}")
-//       .then()
-//       .statusCode(404);
-//   }
-
-//   @Test
-//   void testGetUserAddresses_noCondition_shouldReturnAllUsersAddressesAndStatusOK() {
-//     given()
-//       .basePath("/api/v8")
-//       .pathParam("id", userEntity.getId())
-//       .when().get("/users/{id}/addresses")
-//       .then()
-//       .statusCode(200)
-//       .body("$.size()", equalTo(1));
-//   }
-
-//   @Test
-//   void testCreateUserAddress_givenValidPayload_shouldCreateTheUserAddressAndReturnStatusCreated() {
-//     var street = faker.address().streetAddress();
-//     var city =faker.address().city();
-//     var state =faker.address().state();
-//     var zipCode= faker.address().zipCode();
-//     UserAddress userAddress = new UserAddress(null, street, city, state, zipCode, userEntity.getId());
-//     given()
-//       .basePath("/api/v8")
-//       .contentType(ContentType.JSON)
-//       .pathParam("id", userEntity.getId())
-//       .body(userAddress)
-//       .when().post("/users/{id}/addresses")
-//       .then()
-//       .statusCode(201)
-//       .body("street", equalTo(street))
-//       .body("city", equalTo(city))
-//       .body("state", equalTo(state))
-//       .body("zipCode", equalTo(zipCode));
-//   }
-
-//   @Test
-//   void testUpdateUserAddress_givenValidPayload_shouldUpdateTheUserAddressAndReturnStatusOK() {
-//     var street = faker.address().streetAddress();
-//     var city =faker.address().city();
-//     var state =faker.address().state();
-//     var zipCode= faker.address().zipCode();
-//     UserAddress userAddress = new UserAddress(addressEntity.getId(), street, city, state, zipCode, userEntity.getId());
-//     given()
-//       .basePath("/api/v8")
-//       .contentType(ContentType.JSON)
-//       .pathParam("id", userEntity.getId())
-//       .pathParam("addressId", addressEntity.getId())
-//       .body(userAddress)
-//       .when().put("/users/{id}/addresses/{addressId}")
-//       .then()
-//       .statusCode(200)
-//       .body("street", equalTo(street))
-//       .body("city", equalTo(city))
-//       .body("state", equalTo(state))
-//       .body("zipCode", equalTo(zipCode));
-//   }
-
-//   @Test
-//   void testDeleteUserAddress_givenValidPayload_shouldDeleteTheUserAddressAndReturnStatusNoContent() {
-//     given()
-//       .basePath("/api/v8")
-//       .pathParam("id", userEntity.getId())
-//       .pathParam("addressId", addressEntity.getId())
-//       .when().delete("/users/{id}/addresses/{addressId}")
-//       .then()
-//       .statusCode(204);
-//   }
 }
