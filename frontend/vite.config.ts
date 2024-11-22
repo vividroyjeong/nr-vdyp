a412b596b8f506379625a76b0f02f35eb2c7edde
Roy Jeong <roy.jeong@gov.bc.ca>
2024-11-22 13:23:59 -0800

    Load app-level env vars to node-level env vars

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,10 @@ import packageVersion from 'vite-plugin-package-version'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load environment variables
-  const env = loadEnv(mode, process.cwd(), '')
-  console.log('Loaded VITE_API_URL:', env.VITE_API_URL)
+  // const env = loadEnv(mode, process.cwd(), '')
+  // console.log('Loaded VITE_API_URL:', env.VITE_API_URL)
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  console.log('Loaded VITE_API_URL:', process.env.VITE_API_URL)
 
   return {
     plugins: [
@@ -45,7 +47,8 @@ export default defineConfig(({ mode }) => {
       proxy: {
         // Proxy API requests to the backend
         '/api': {
-          target: env.VITE_API_URL,
+          // target: env.VITE_API_URL,
+          target: process.env.VITE_API_URL,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
@@ -53,7 +56,8 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      // 'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
     },
   }
 })
