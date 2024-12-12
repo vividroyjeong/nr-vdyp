107ffc3f7b745a8d09b3dd577cc986a69b647216
Roy Jeong <roy.jeong@gov.bc.ca>
2024-12-11 21:57:24 -0800

    Delete unused dcsvprojection/scsvprojection api client libraries

--- frontend/src/services/vdyp-api/api.ts
+++ frontend/src/services/vdyp-api/api.ts
@@ -1,5 +1,3 @@
 export * from './apis/get-help-api'
 export * from './apis/get-root-api'
-export * from './apis/run-dcsvprojection-api'
 export * from './apis/run-hcsvprojection-api'
-export * from './apis/run-scsvprojection-api'
